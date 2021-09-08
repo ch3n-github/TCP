f7887484b547226eeba5cb5032ae1c5c2ae6c5fb
ch3n-github <61458959+ch3n-github@users.noreply.github.com>
2021-09-08 19:20:40 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -6,43 +6,46 @@ import cv2
 import numpy
 import struct
 
-
+#端口
 PORT = 12340
+#服务器地址
 HOST = socket.gethostname()
 
 
-
+#客户端传输函数
 def tcp_client(port,cap):
 	
 	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	# 连接服务端
-
-	
+	# 连接服务端	
 	address_server = (HOST, 12340)
 	sock.connect(address_server)
+	#读取视频流
 	ret, frame = cap.read()
+	#图片编码参数
 	encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 	while ret:
-
+		#图像编码
 		result, imgencode = cv2.imencode('.jpg', frame)
 		data = numpy.array(imgencode)
 		stringData = data.tobytes()
-
+		#打包传输图像长度
 		len_byte = struct.pack('i',len(stringData))
+		#传输长度
 		sock.send(len_byte)
+		#传输图像
 		sock.send(stringData)
+		#读取视频流
 		ret, frame = cap.read()
-
 
 	cap.release()
 	cv2.destroyAllWindows()
 
 
-
-
 def main():
+	#设定视频流
 	cap = cv2.VideoCapture(0)
-
+	#客户端传输函数
 	tcp_client(PORT,cap)
+
 if __name__ == '__main__':
 	main()
--- server.py
+++ server.py
@@ -6,23 +6,28 @@ import numpy
 import struct
 import time
 from threading import Thread
-
+#端口
 PORT = 12340
-
+#服务端接收函数
 def tcp_server(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = ""
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #服务端地址端口设定
+    host = ""
     address = (host, port)
+    #绑定地址
     s.bind(address) 
+    #多线程监听
     s.listen(5)
     print ('Waiting for input...')
+    #阻断等待连接
     conn, addr = s.accept()
+    #缓存
     time.sleep(2)
-
 
     while 1:
         try:
+            #接收图像长度
             head = conn.recv(4)
             if head:
                 length = struct.unpack('i',head)[0]
